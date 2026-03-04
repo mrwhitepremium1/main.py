7e99f5228be0d979b82d9ec81fd1aa8b3a303cf5
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-04 18:43:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
 
+# Telegram bot
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+
+# Admins
+ADMIN_IDS = [int(os.environ.get("ADMIN_ID"))]
+
+# Payment
+PAYSTACK_SECRET_KEY = os.environ.get("PAYSTACK_SECRET_KEY")
+
+# Ticket (image or PDF URL)
+TICKET_URL = os.environ.get("TICKET_URL")
