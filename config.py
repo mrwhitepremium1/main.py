d5deb0e251c047dd04f15197ee209dab4c0ca047
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-04 21:59:12 +0000

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 PAYSTACK_SECRET_KEY = os.environ.get("PAYSTACK_SECRET_KEY")
 TICKET_URL = os.environ.get("TICKET_URL")
-ADMIN_ID = int(os.environ.get("ADMIN_ID"))  # Replace with your Telegram ID
+ADMIN_ID = int(os.environ.get("ADMIN_ID"))  # Your Telegram ID
