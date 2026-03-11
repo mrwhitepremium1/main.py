c10ee12667a67200eae0b4c31e554d84f73c4e2a
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-11 17:20:57 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
-PAYSTACK_SECRET_KEY = os.environ.get("PAYSTACK_SECRET_KEY")
+SELAR_PAYMENT_LINK = https://selar.com/4w57915757
 TICKET_URL = os.environ.get("TICKET_URL")
 ADMIN_ID = int(os.environ.get("ADMIN_ID"))  # Your Telegram ID
