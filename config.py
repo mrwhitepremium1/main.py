64315f33987278c90d823936c9582ff2c07d5297
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-11 18:25:27 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-# API Credentials
-API_ID = os.environ.get("API_ID")
+# Telegram Credentials
+API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-# Personal Settings
+# Admin & Links
 ADMIN_ID = int(os.environ.get("ADMIN_ID"))
 SELAR_PAYMENT_LINK = "https://selar.com/mrwhite"
 
-# Images
+# Images (Managed via Railway Variables)
 WELCOME_IMAGE = "https://github.com/mrwhitepremium1/main.py/raw/refs/heads/main/IMG_9460.png"
-TICKET_IMAGE = os.environ.get("TICKET_URL") # Set this in Railway variables
+TICKET_IMAGE = os.environ.get("TICKET_URL") 
