ae3d62c1ab8922fb9a1c20c6c7ae80f3b7c239d8
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-11 17:28:43 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,14 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
-SELAR_PAYMENT_LINK = https://selar.com/4w57915757
-TICKET_URL = os.environ.get("TICKET_URL")
+
 ADMIN_ID = int(os.environ.get("ADMIN_ID"))  # Your Telegram ID
+
+# Selar product checkout link
+SELAR_PAYMENT_LINK = "https://selar.com/4w57915757"
+
+# Ticket image (can change daily)
+TICKET_IMAGE = os.environ.get("TICKET_URL")
+
+# Selar webhook secret
+SELAR_SECRET = "YOUR_SELAR_WEBHOOK_SECRET"
