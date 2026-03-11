d84ec9017aa678c4ab7c8d017bf3c49ed6ee1339
mrwhitepremium1 <mrwhitepremium1@gmail.com>
2026-03-11 17:56:04 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os
+# Ticket image (can change daily)
+TICKET_IMAGE = os.environ.get("TICKET_URL")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 
 ADMIN_ID = int(os.environ.get("ADMIN_ID"))  # Your Telegram ID
 
-# Selar product checkout link
-SELAR_PAYMENT_LINK = "https://selar.com/4w57915757"
+SELAR_PAYMENT_LINK = "https://selar.com/mrwhite"
 
-# Ticket image (can change daily)
-TICKET_IMAGE = os.environ.get("TICKET_URL")
+WELCOME_IMAGE = "https://github.com/mrwhitepremium1/main.py/raw/refs/heads/main/IMG_9460.png"
